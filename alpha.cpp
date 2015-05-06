b215fa6acb1d3ddd71c6e15bfaee3a186f220acc
Allen <allensratheburn@gmail.com>
2015-05-06 14:33:18 -0400

    p1

---
+++ alpha.cpp
@@ -0,0 +1,40 @@
+#ifndef MY_ALPHA
+#define MY_ALPHA
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	char letter;
+	const int matrix = 9;
+	char lar[matrix][matrix];
+
+	//init_matrix();
+
+	for(int i = 0; i < matrix; i++)
+	{
+		for(int j = 0; j < matrix; j++)
+		{
+			lar[i][j] = 'S';
+		}
+	}
+
+	for(int i = 0; i < matrix; i++)
+	{
+		for(int j = 0; j < matrix; j++)
+		{
+			cout << lar[i][j] << " " << "\n";
+		}
+	}
+
+	
+
+	//populate_matrix(letter);
+
+	return 0;
+}
+
+
+#endif
